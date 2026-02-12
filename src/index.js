65428a8f66e498f1fb6689dbd4c302e2fc0d683d
matheus3070 <matheeus.estudos2002@gmail.com>
2026-02-12 16:07:54 -0300

    desafio realizado, introduzido função e estrutura switch case

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-console.log("matheus");
+let saldoVitorias = 90;
+let saldoDerrotas = 5;
 
-console.log("teste");
+let saldoRankeada = saldo(saldoVitorias, saldoDerrotas);
+
+function saldo(vitorias, derrotas){
+    return vitorias - derrotas;
+}
+
+
+switch (true){
+    case saldoRankeada <= 10:
+        console.log(`O herói tem um saldo de ${saldoRankeada} está no nível de Ferro`);
+        break;
+
+    case saldoRankeada >= 11 && saldoRankeada <= 20:
+        console.log(`O herói tem um saldo de ${saldoRankeada} está no nível de Bronze`);
+        break;
+
+    case saldoRankeada >= 21 && saldoRankeada <= 50:
+        console.log(`O herói tem um saldo de ${saldoRankeada} está no nível de Prata`);
+        break;
+
+    case saldoRankeada >= 51 && saldoRankeada <= 80:
+        console.log(`O herói tem um saldo de ${saldoRankeada} está no nível de Ouro`);
+        break;
+
+    case saldoRankeada >= 81 && saldoRankeada <= 90:
+        console.log(`O herói tem um saldo de ${saldoRankeada} está no nível de Diamante`);
+        break;
+
+    case saldoRankeada >= 91 && saldoRankeada <= 100:
+        console.log(`O herói tem um saldo de ${saldoRankeada} está no nível de Lendário`);
+        break;
+
+    default:
+        console.log(`O herói tem um saldo de ${saldoRankeada} está no nível de Imortal`);
+
+}
